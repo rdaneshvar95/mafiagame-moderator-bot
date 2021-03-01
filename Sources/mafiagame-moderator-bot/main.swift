dc89b3c117190327f14a6e9b7c454e007e6930cd
Reza Daneshvar <rdaneshvar95@gmail.com>
2021-03-01 22:11:56 +0100

    connected to telegram bot

--- Sources/mafiagame-moderator-bot/main.swift
+++ Sources/mafiagame-moderator-bot/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import TelegramBotSDK
+
+let token = readToken(from: "MAFIAGAME_MODERATOR_BOT_TOKEN")
+let bot = TelegramBot(token: token)
+
+let players: [User] = []
+
+while let update = bot.nextUpdateSync() {
+    if let message = update.message, let from = message.from, let text = message.text {
+        bot.sendMessageAsync(chatId: .chat(from.id),
+                             text: "Hi \(from.firstName)! You said: \(text).\n")
+    }
+}
+
+fatalError("Server stopped due to error: \(String(describing: bot.lastError))")
